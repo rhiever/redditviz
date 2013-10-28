c93a0aefeab2e4cb4bad190e7cc55b7602430c10
Randy Olson <rso@randalolson.com>
2013-10-28 01:43:52 -0400

    json debugging

--- js/main.js
+++ js/main.js
@@ -677,34 +677,29 @@ function nodeActive(a) {
         }
 
 		// pull info about the activated subreddit from reddit
-		var SRimage = null;
-		var SRdesc = "";
-		
 		jQuery.getJSON("http://www.reddit.com/r/" + b.label + "/about.json?jsonp=?",
 			function parse(data)
 			{
-				SRimage = data.data.header_img;
-				SRdesc = data.data.public_description;
+				var SRimage = data.data.header_img;
+				var SRdesc = data.data.public_description;
+				
+				$('#subreddit-logo').attr('src', SRimage);
+				$('#subreddit-logo').attr('alt', b.label);
+				$('#subreddit-logo').attr('title', b.label);
+
+				if (image_attribute) {
+					//image_index = jQuery.inArray(image_attribute, temp_array);
+					$GP.info_name.html("<div><img src=" + f.attributes[image_attribute] + " style=\"vertical-align:middle\" /> <span onmouseover=\"sigInst._core.plotter.drawHoverNode(sigInst._core.graph.nodesIndex['" + b.id + '\'])" onmouseout="sigInst.refresh()">' + b.label + "</span></div>");
+				} else {
+					$GP.info_name.html("<div><span onmouseover=\"sigInst._core.plotter.drawHoverNode(sigInst._core.graph.nodesIndex['" + b.id + '\'])" onmouseout="sigInst.refresh()"><a target="_blank" title="Go to /r/' + b.label + '" href="http://reddit.com/r/' + b.label + '/">' + b.label + ' <i class="icon-external-link"></i></a><br />' + SRdesc + '</span></div>');
+				}
+				// Image field for attribute pane
+				$GP.info_data.html(e.join("<br/>"));
 			}
         )
         .success(function() { ; })
-        .error(function() { if (SRimage == null) { SRimage = "http://metareddit.com/static/logos/" + b.label + ".png"; } })
-        .complete(function() { if (SRdesc == null) { SRdesc = "";} });
-		
-        $('#subreddit-logo').attr('src', SRimage);
-        $('#subreddit-logo').attr('alt', b.label);
-        $('#subreddit-logo').attr('title', b.label);
-		
-		alert(SRimage + "\n" + SRdesc);
-
-        if (image_attribute) {
-        	//image_index = jQuery.inArray(image_attribute, temp_array);
-        	$GP.info_name.html("<div><img src=" + f.attributes[image_attribute] + " style=\"vertical-align:middle\" /> <span onmouseover=\"sigInst._core.plotter.drawHoverNode(sigInst._core.graph.nodesIndex['" + b.id + '\'])" onmouseout="sigInst.refresh()">' + b.label + "</span></div>");
-        } else {
-        	$GP.info_name.html("<div><span onmouseover=\"sigInst._core.plotter.drawHoverNode(sigInst._core.graph.nodesIndex['" + b.id + '\'])" onmouseout="sigInst.refresh()"><a target="_blank" title="Go to /r/' + b.label + '" href="http://reddit.com/r/' + b.label + '/">' + b.label + ' <i class="icon-external-link"></i></a><br />' + SRdesc + '</span></div>');
-        }
-        // Image field for attribute pane
-        $GP.info_data.html(e.join("<br/>"));
+        .error(function() { ; })
+        .complete(function() { ; });
     }
     sigInst._core.plotter.drawHoverNode(sigInst._core.graph.nodesIndex[b.id]);  // Highlight the current node
     $GP.info_data.show();
