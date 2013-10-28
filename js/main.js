f2a89b653958d8fd80fe534c5a0b763bc8e31296
Randy Olson <rso@randalolson.com>
2013-10-28 01:44:55 -0400

    formatting text

--- js/main.js
+++ js/main.js
@@ -691,7 +691,7 @@ function nodeActive(a) {
 					//image_index = jQuery.inArray(image_attribute, temp_array);
 					$GP.info_name.html("<div><img src=" + f.attributes[image_attribute] + " style=\"vertical-align:middle\" /> <span onmouseover=\"sigInst._core.plotter.drawHoverNode(sigInst._core.graph.nodesIndex['" + b.id + '\'])" onmouseout="sigInst.refresh()">' + b.label + "</span></div>");
 				} else {
-					$GP.info_name.html("<div><span onmouseover=\"sigInst._core.plotter.drawHoverNode(sigInst._core.graph.nodesIndex['" + b.id + '\'])" onmouseout="sigInst.refresh()"><a target="_blank" title="Go to /r/' + b.label + '" href="http://reddit.com/r/' + b.label + '/">' + b.label + ' <i class="icon-external-link"></i></a><br />' + SRdesc + '</span></div>');
+					$GP.info_name.html("<div><span onmouseover=\"sigInst._core.plotter.drawHoverNode(sigInst._core.graph.nodesIndex['" + b.id + '\'])" onmouseout="sigInst.refresh()"><a target="_blank" title="Go to /r/' + b.label + '" href="http://reddit.com/r/' + b.label + '/">' + b.label + ' <i class="icon-external-link"></i></a><br /><br />' + SRdesc + '</span></div>');
 				}
 				// Image field for attribute pane
 				$GP.info_data.html(e.join("<br/>"));
